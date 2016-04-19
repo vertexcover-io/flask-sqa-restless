dff2a431e2fd713c1a3c4b10dfe64c21223d34d5
Ritesh Kadmawala <ritesh@voxapp.com>
2016-04-19 19:43:54 +0530

    Minor setup fixes

--- setup.py
+++ setup.py
@@ -11,8 +11,22 @@ except ImportError:
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+def get_absolute_path(*args):
+    """Transform relative pathnames into absolute pathnames."""
+    directory = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(directory, *args)
+
+
+def get_version():
+    """Get the version of `package` (by extracting it from the source code)."""
+    module_path = get_absolute_path('flas', '__init__.py')
+    with open(module_path) as handle:
+        for line in handle:
+            match = re.match(r'^__version__\s*=\s*["\']([^"\']+)["\']$', line)
+            if match:
+                return match.group(1)
+    raise Exception("Failed to extract version from %s!" % module_path)
+
 
 requirements = [
 ]
@@ -23,34 +37,43 @@ test_requirements = [
 
 setup(
     name='flask-sqa-restless',
-    version='0.0.1',
+    version=get_version(),
     description="Flask REST API Framework for SQLAlchemy",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author="Ritesh Kadmawala",
-    author_email='k.g.ritesh@gmail.com',
-    url='https://github.com/kgritesh/flask-sqa-restless',
+    author_email='ritesh@loanzen.in',
+    url='https://github.com/loanzen/flask_sqa_restless',
     packages=[
-        'flask-sqa-restless',
+        'flask_sqa_restless',
     ],
-    package_dir={'flask-sqa-restless':
-                 'flask-sqa-restless'},
+    package_dir={'flask_sqa_restless':
+                 'flask_sqa_restless'},
     include_package_data=True,
     install_requires=requirements,
     license="ISCL",
     zip_safe=False,
-    keywords='flask-sqa-restless',
+    keywords='flask-sqa-restless, REST Framework',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Information Technology',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development:: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
     ],
     test_suite='tests',
     tests_require=test_requirements
--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'kgritesh/flask-sqa-restless'
+GITHUB_REPO = 'loanzen/flask-sqa-restless'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
